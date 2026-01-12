d1d5631fbfb8ea1401a045fffe2d20a44c12af1d
Daith-42 <david.ameline42@gmail.com>
2026-01-12 11:20:49 +0100

    input function fix

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
-#include <stdio.h>;
-#include <stdlib.h>;
+#include <stdio.h>
+#include <stdlib.h>
 
 int input_function(void);
 int position_available(int x, int y, int tab);
+int affichage(int **tab);
 
 int main() {
+    int sudiku[9][9] = {
+        {2, 8, 5, 6, 1, 3, 9, 4, 7},
+        {4, 9, 3, 8, 7, 2, 5, 1, 6},
+        {1, 6, 7, 4, 5, 9, 8, 2, 3},
+        {3, 1, 9, 2, 6, 5, 4, 7, 8},
+        {6, 4, 8, 7, 9, 1, 3, 5, 2},
+        {5, 7, 2, 3, 8, 4, 1, 6, 9},
+        {8, 3, 6, 1, 4, 7, 2, 9, 5},
+        {7, 5, 1, 9, 2, 8, 6, 3, 4},
+        {9, 2, 4, 5, 3, 6, 7, 8, 1}
+    };
     printf("hello world");
-    input_function();
+    int x, y =  input_function();
+    affichage(tab[9][9]);
     return 0;
 }
 
 int input_function(void) {
-    int X;
-    int Y;
+    int x;
+    int y;
     printf("Give me 2 number\n");
     do {
         printf("Give me x: ");
@@ -24,7 +37,7 @@ int input_function(void) {
         int value = strtof(input,&endPtr);
 
         if (input!=endPtr && value>0 && value<10) {
-            X = value;
+            x = value;
             break;
         }
     }while (1);
@@ -39,10 +52,11 @@ int input_function(void) {
         int value = strtof(input,&endPtr);
 
         if (input!=endPtr && value>0 && value<10) {
-            Y = value;
+            y = value;
             break;
         }
     }while (1);
+    return x, y;
 }
 
 int position_available(int x, int y, int tab) {
@@ -50,4 +64,20 @@ int position_available(int x, int y, int tab) {
         return 1;
     }
     return 0;
+}
+
+int affichage(int **tab) {
+    for (int i = 0; i < 8; i++) {
+        printf("\n");
+        for (int j = 0; j < 8; j++) {
+            if (tab[i][j] == 0) {
+                printf("%c", '.');
+            } else {
+                printf("%d ", tab[i][j]);
+            }
+            printf("------------------\n");
+        }
+    }
+
+
 }
